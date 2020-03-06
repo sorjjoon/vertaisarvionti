9735eadf595dc65274a568a7a09f8658fbf33b7a
joona <joona.sorjonen97@gmail.com>
2020-03-06 20:47:55 +0200

    initial heroku

--- app/__init__.py
+++ app/__init__.py
@@ -8,15 +8,15 @@ app = Flask(__name__)
 
 
 # login
-from flask_login import LoginManager
+#from flask_login import LoginManager
 
 app.config["SECRET_KEY"] = os.urandom(32)
 
-login_manager = LoginManager()
-login_manager.init_app(app)
+# login_manager = LoginManager()
+# login_manager.init_app(app)
 
-login_manager.login_view = "login_auth"
-login_manager.login_message = "Please login to use this functionality."
+# login_manager.login_view = "login_auth"
+# login_manager.login_message = "Please login to use this functionality."
 
 
 from app import views
